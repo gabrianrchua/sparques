ed9a3d9ddc7ce0d6211b1b3711acf7de003c4af6
Gabrian Chua <gabrianrchua@gmail.com>
2025-01-15 20:41:43 -0500

    feat(wip): canvas module functions for brush and circle

--- canvas/draw.js
+++ canvas/draw.js
@@ -1,3 +1,37 @@
+/**
+ * Draw a brush stroke with an array of coordinates
+ * @param {CanvasRenderingContext2D} ctx Canvas context 2d
+ */
 export function brush(ctx, {color, width, coordinates}) {
-  console.log("brush", ctx, color, width, coordinates);
+  if (!ctx || !color || !width || !coordinates || coordinates.length == 0) throw new Error("Invalid arguments");
+  
+  ctx.strokeStyle = color;
+  ctx.lineWidth = width;
+
+  ctx.beginPath();
+
+  for (let i = 0; i < coordinates.length; i++) {
+    if (i == 0) {
+      ctx.moveTo(coordinates[0].x, coordinates[0].y);
+    } else {
+      ctx.lineTo(coordinates[i].x, coordinates[i].y);
+    }
+  }
+
+  ctx.stroke();
+}
+
+/**
+ * Draw a circle
+ * @param {CanvasRenderingContext2D} ctx Canvas context 2d
+ */
+export function circle(ctx, {color, width, center, radius}) {
+  if (!ctx || !color || !width || !center || !center.x || !center.y || !radius) throw new Error("Invalid arguments");
+
+  ctx.strokeStyle = color;
+  ctx.lineWidth = width;
+
+  ctx.beginPath();
+  ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
+  ctx.stroke();
 }
--- canvas/test.js
+++ canvas/test.js
@@ -1,10 +1,13 @@
-import { brush } from "./draw.js"
+import { brush, circle } from "./draw.js"
+
+// CLIENT SIDE DRIVER CODE TEST
 
 let ctx;
 
 export function init(canvas) {
   ctx = canvas.getContext("2d");
-  brush(ctx, { color: "#000000", width: 3, coordinates: [
+  brush(ctx, { color: "#ff0000", width: 3, coordinates: [
     {x: 10, y: 10}, {x: 10, y: 50}, {x: 50, y: 50}, {x: 100, y: 100}
   ]});
+  circle(ctx, {color: "#00ff00", width: 5, center: {x: 100, y: 100}, radius: 50});
 }
