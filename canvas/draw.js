43c28fb5bdbc85f7708128f19d47d07ac4755e38
Gabrian Chua <gabrianrchua@gmail.com>
2025-01-16 12:17:41 -0500

    feat: complete draw functions module

--- canvas/draw.js
+++ canvas/draw.js
@@ -2,9 +2,9 @@
  * Draw a brush stroke with an array of coordinates
  * @param {CanvasRenderingContext2D} ctx Canvas context 2d
  */
-export function brush(ctx, {color, width, coordinates}) {
+export function brush(ctx, { color, width, coordinates }) {
   if (!ctx || !color || !width || !coordinates || coordinates.length == 0) throw new Error("Invalid arguments");
-  
+
   ctx.strokeStyle = color;
   ctx.lineWidth = width;
 
@@ -25,7 +25,7 @@ export function brush(ctx, {color, width, coordinates}) {
  * Draw a circle
  * @param {CanvasRenderingContext2D} ctx Canvas context 2d
  */
-export function circle(ctx, {color, width, center, radius}) {
+export function circle(ctx, { color, width, center, radius }) {
   if (!ctx || !color || !width || !center || !center.x || !center.y || !radius) throw new Error("Invalid arguments");
 
   ctx.strokeStyle = color;
@@ -34,4 +34,167 @@ export function circle(ctx, {color, width, center, radius}) {
   ctx.beginPath();
   ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
   ctx.stroke();
+}
+
+/**
+ * Draw a rectangle
+ * @param {CanvasRenderingContext2D} ctx Canvas context 2d
+ */
+export function rectangle(ctx, { color, width, topLeftCoordinates, bottomRightCoordinates }) {
+  if (!ctx || !color || !width || !topLeftCoordinates || !topLeftCoordinates.x || !topLeftCoordinates.y || !bottomRightCoordinates || !bottomRightCoordinates.x || !bottomRightCoordinates.y) throw new Error("Invalid arguments");
+
+  ctx.strokeStyle = color;
+  ctx.lineWidth = width;
+
+  ctx.strokeRect(topLeftCoordinates.x, topLeftCoordinates.y, Math.abs(topLeftCoordinates.x - bottomRightCoordinates.x), Math.abs(topLeftCoordinates.y - bottomRightCoordinates.y));
+}
+
+/**
+ * Draw an n-sided regular polygon
+ * @param {CanvasRenderingContext2D} ctx Canvas context 2d
+ */
+export function polygon(ctx, { color, width, numSides, center, sideLength }) {
+  if (!color || !width || !numSides || numSides < 3 || !center || !center.x || !center.y || !sideLength) throw new Error("Invalid arguments");
+
+  ctx.strokeStyle = color;
+  ctx.lineWidth = width;
+
+  // https://stackoverflow.com/a/11354824
+  ctx.beginPath();
+  ctx.moveTo(center.x + sideLength * Math.cos(0), center.y + sideLength * Math.sin(0));
+
+  for (let i = 1; i <= numSides; i++) {
+    ctx.lineTo(center.x + sideLength * Math.cos(i * 2 * Math.PI / numSides), center.y + sideLength * Math.sin(i * 2 * Math.PI / numSides));
+  }
+
+  ctx.stroke();
+}
+
+/**
+ * Write text
+ * @param {CanvasRenderingContext2D} ctx Canvas context 2d
+ */
+export function text(ctx, { color, fontSize, topLeftCoordinates, text }) {
+  if (!color || !fontSize || !topLeftCoordinates || !topLeftCoordinates.x || !topLeftCoordinates.y || !text) throw new Error("Invalid arguments");
+
+  ctx.font = `${fontSize}px sans-serif`;
+  ctx.fillStyle = color;
+
+  ctx.fillText(text, topLeftCoordinates.x, topLeftCoordinates.y, Math.abs(topLeftCoordinates.x - 512));
+}
+
+/*
+  Flood fill code below courtesy of https://stackoverflow.com/a/56221940
+*/
+
+/**
+ * Flood (bucket) fill at a section of the canvas
+ * @param {CanvasRenderingContext2D} ctx Canvas context 2d
+ */
+export function fill(ctx, { color, coordinates }) {
+  if (!color || !coordinates || !coordinates.x || !coordinates.y) throw new Error("Invalid arguments");
+
+  const x = coordinates.x;
+  const y = coordinates.y;
+  color = parseInt("0xFF" + color.replace("#", ""), 16);
+
+  // read the pixels in the canvas
+  const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  // make a Uint32Array view on the pixels so we can manipulate pixels
+  // one 32bit value at a time instead of as 4 bytes per pixel
+  const pixelData = {
+    width: imageData.width,
+    height: imageData.height,
+    data: new Uint32Array(imageData.data.buffer),
+  };
+
+  // get the color we're filling
+  const targetColor = getPixel(pixelData, x, y);
+
+  // check we are actually filling a different color
+  if (targetColor !== color) {
+    const spansToCheck = [];
+
+    function addSpan(left, right, y, direction) {
+      spansToCheck.push({ left, right, y, direction });
+    }
+
+    function checkSpan(left, right, y, direction) {
+      let inSpan = false;
+      let start;
+      let x;
+      for (x = left; x < right; ++x) {
+        const color = getPixel(pixelData, x, y);
+        if (color === targetColor) {
+          if (!inSpan) {
+            inSpan = true;
+            start = x;
+          }
+        } else {
+          if (inSpan) {
+            inSpan = false;
+            addSpan(start, x - 1, y, direction);
+          }
+        }
+      }
+      if (inSpan) {
+        inSpan = false;
+        addSpan(start, x - 1, y, direction);
+      }
+    }
+
+    addSpan(x, x, y, 0);
+
+    while (spansToCheck.length > 0) {
+      const { left, right, y, direction } = spansToCheck.pop();
+
+      // do left until we hit something, while we do this check above and below and add
+      let l = left;
+      for (; ;) {
+        --l;
+        const color = getPixel(pixelData, l, y);
+        if (color !== targetColor) {
+          break;
+        }
+      }
+      ++l
+
+      let r = right;
+      for (; ;) {
+        ++r;
+        const color = getPixel(pixelData, r, y);
+        if (color !== targetColor) {
+          break;
+        }
+      }
+
+      const lineOffset = y * pixelData.width;
+      pixelData.data.fill(color, lineOffset + l, lineOffset + r);
+
+      if (direction <= 0) {
+        checkSpan(l, r, y - 1, -1);
+      } else {
+        checkSpan(l, left, y - 1, -1);
+        checkSpan(right, r, y - 1, -1);
+      }
+
+      if (direction >= 0) {
+        checkSpan(l, r, y + 1, +1);
+      } else {
+        checkSpan(l, left, y + 1, +1);
+        checkSpan(right, r, y + 1, +1);
+      }
+    }
+    // put the data back
+    ctx.putImageData(imageData, 0, 0);
+  }
+}
+
+function getPixel(pixelData, x, y) {
+  if (x < 0 || y < 0 || x >= pixelData.width || y >= pixelData.height) {
+    return -1;  // impossible color
+  } else {
+    return pixelData.data[y * pixelData.width + x];
+  }
 }
--- canvas/test.js
+++ canvas/test.js
@@ -1,4 +1,4 @@
-import { brush, circle } from "./draw.js"
+import { brush, circle, fill, polygon, rectangle, text } from "./draw.js"
 
 // CLIENT SIDE DRIVER CODE TEST
 
@@ -6,8 +6,16 @@ let ctx;
 
 export function init(canvas) {
   ctx = canvas.getContext("2d");
-  brush(ctx, { color: "#ff0000", width: 3, coordinates: [
-    {x: 10, y: 10}, {x: 10, y: 50}, {x: 50, y: 50}, {x: 100, y: 100}
-  ]});
-  circle(ctx, {color: "#00ff00", width: 5, center: {x: 100, y: 100}, radius: 50});
+  brush(ctx, {
+    color: "#ff0000", width: 3, coordinates: [
+      { x: 10, y: 10 }, { x: 10, y: 50 }, { x: 50, y: 50 }, { x: 100, y: 100 }
+    ]
+  });
+  circle(ctx, { color: "#00ff00", width: 5, center: { x: 100, y: 100 }, radius: 50 });
+  rectangle(ctx, { color: "#0000ff", width: 2, topLeftCoordinates: { x: 100, y: 100 }, bottomRightCoordinates: { x: 200, y: 150 } });
+  polygon(ctx, { color: "#ffff00", width: 5, center: { x: 300, y: 300 }, numSides: 5, sideLength: 50 });
+  polygon(ctx, { color: "#00ffff", width: 5, center: { x: 350, y: 350 }, numSides: 8, sideLength: 50 });
+  text(ctx, { color: "#808080", fontSize: 20, topLeftCoordinates: { x: 50, y: 400 }, text: "my text" });
+  text(ctx, { color: "#ff00ff", fontSize: 20, topLeftCoordinates: { x: 300, y: 400 }, text: "some very long text. i'm not sure if it'll wrap (probably not). hopefully my math is right." });
+  fill(ctx, { color: "#ff00ff", coordinates: { x: 90, y: 100 } });
 }
