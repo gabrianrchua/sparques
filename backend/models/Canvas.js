76c9976b7b8884839052e9ca73e74847fc95fdc4
Gabrian Chua <gabrianrchua@gmail.com>
2025-01-19 14:55:24 -0500

    feat: add true blank base image

--- backend/models/Canvas.js
+++ backend/models/Canvas.js
@@ -4,7 +4,7 @@ const { strokeSchema } = require('./Stroke');
 const canvasSchema = new mongoose.Schema({
   title: { type: String, required: true, unique: true },
   strokes: { type: [strokeSchema], default: [] },
-  baseImage: { type: String, required: true }, // image/bmp 512x512 8-bit RGB color ~= 786 kB
+  baseImage: { type: String, required: true }, // image/png 512x512
 });
 
 module.exports = mongoose.model("Canvas", canvasSchema);
