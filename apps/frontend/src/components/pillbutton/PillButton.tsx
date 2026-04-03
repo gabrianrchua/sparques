785407ec32fa7ae5dd60f6e3efbe666af87a1616
Gabrian Chua <gabrianrchua@gmail.com>
2026-04-03 19:18:27 -0400

    lint: run linter

--- apps/frontend/src/components/pillbutton/PillButton.tsx
+++ apps/frontend/src/components/pillbutton/PillButton.tsx
@@ -1,17 +1,17 @@
-import { Button, ButtonProps, styled } from "@mui/material";
+import { Button, ButtonProps, styled } from '@mui/material';
 
-const PillButton = styled(Button)<ButtonProps>(props => {
-  return ({
-    color: props.color || "white",
-    backgroundColor: "#00000",
+const PillButton = styled(Button)<ButtonProps>((props) => {
+  return {
+    color: props.color || 'white',
+    backgroundColor: '#00000',
     '&:hover': {
-      backgroundColor: "#333333",
+      backgroundColor: '#333333',
     },
-    border: "1px solid #808080",
-    borderRadius: "30px",
-    height: "36px",
-    marginRight: "10px",
-  });
+    border: '1px solid #808080',
+    borderRadius: '30px',
+    height: '36px',
+    marginRight: '10px',
+  };
 });
 
 export default PillButton;
