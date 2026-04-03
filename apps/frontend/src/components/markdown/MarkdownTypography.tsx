a3f12d0e9cde3b38b4476926d4e96b1309718ac4
Gabrian Chua <gabrianrchua@gmail.com>
2026-04-03 19:45:23 -0400

    feat: add background to markdowncode

--- apps/frontend/src/components/markdown/MarkdownTypography.tsx
+++ apps/frontend/src/components/markdown/MarkdownTypography.tsx
@@ -1,12 +1,18 @@
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import styles from './MarkdownTypography.module.css';
+import { Box } from '@mui/material';
 
 interface MarkdownTypographyProps {
   text: string;
 }
 
 const MarkdownTypography = ({ text }: MarkdownTypographyProps) => {
-  return <Markdown remarkPlugins={[remarkGfm]}>{text}</Markdown>;
+  return (
+    <Box className={styles.markdownWrapper}>
+      <Markdown remarkPlugins={[remarkGfm]}>{text}</Markdown>
+    </Box>
+  );
 };
 
 export default MarkdownTypography;
